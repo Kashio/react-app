c2f3824cbcd836491af95661e433944160d82be8
Roy Mor <roy.mor173@gmail.com>
2019-03-18 22:10:24 +0200

    add server routes for login and update status with mongoDB

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,58 @@
-const axios = require('axios');
-const config = require('../config');
+const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
 
-module.exports = {
+const UserSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Working'
+    },
+    logged: {
+        type: Boolean,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+});
 
+UserSchema.statics.login = function ({username}) {
+    return User
+        .findOne({
+            username
+        })
+        .then(result => {
+            if (result) {
+                return result._doc;
+            } else {
+                const user = {
+                    _id: uuidv1(),
+                    username,
+                    status: 'Working',
+                    logged: true,
+                    createAt: new Date()
+                };
+                return User.create(user);
+            }
+        });
 };
+
+UserSchema.statics.updateStatus = function ({_id, status}) {
+    return User.findOneAndUpdate({_id}, {$set: {status}});
+};
+
+const connection = mongoose.createConnection('mongodb://localhost:27017/app');
+const User = connection.model('User', UserSchema);
+
+module.exports = User;
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,10 +3,20 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-router.post('/', (req, res) => {
-    User.create(req.body)
+router.post('/login', (req, res) => {
+    User.login(req.body)
         .then(result => {
-            res.status(result.status).send(result.data);
+            res.status(200).json(result);
+        })
+        .catch(error => {
+            res.status(500).send(error.message);
+        });
+});
+
+router.put('/status', (req, res) => {
+    User.updateStatus(req.body)
+        .then(result => {
+            res.status(200).json(result);
         })
         .catch(error => {
             res.status(500).send(error.message);
